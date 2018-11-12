3f0377e2b21fe0fb4025529fcbdb8658b22cc657
Bayon Forte <bayon@forteworks.com>
2018-11-12 09:07:33 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+/* Thanks to Mae Capozzi*/
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      username: ''
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick () {
+    console.log('Success!')
+    axios.get('https://api.github.com/users/bayon')
+    .then(response =>  this.setState({username: response.data.name}))
+  }
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='button__container'>
+        <button className='button'  onClick={this.handleClick}>Click Me</button>
+        <p>{this.state.username}</p>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
